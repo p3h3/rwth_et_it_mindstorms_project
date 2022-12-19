36d0240021efb68cc20dfc5cb916298a0db2ac70
Peter <hi@p3h3.de>
2022-12-19 08:45:46 +0100

    alles was wir freitag hatten

---
+++ versuch6/dreif.m
@@ -0,0 +1,131 @@
+function dreif()
+    handle = EV3();
+    
+    handle.connect("usb");
+    s = handle.sensor4;
+    
+    m1 = handle.motorA;
+
+    mb = handle.motorB;
+    mc = handle.motorC;
+    
+    m1.limitMode = 'Tacho';
+    m1.limitValue = 360 * 15;
+    m1.power = 15;
+    m1.brakeMode = 'Brake';
+
+    m1.resetTachoCount();
+
+    values = zeros(1,2);
+
+
+    tic;
+
+    m1.start();
+
+    while handle.motorA.tachoCount < 360 * 15
+        index = length(values) + 1;
+        sens_val = handle.sensor4.value;
+        values(index, 1) = sens_val;
+        values(index, 3) = deg2rad(double(handle.motorA.tachoCount / 15));
+        values(index, 2) = toc;
+    end
+
+    m1.stop();
+
+    pause(0.5);
+
+
+    m1.limitValue = 360 * 15;
+    m1.power = -80;
+    m1.brakeMode = 'Brake';
+
+    m1.start();
+    m1.waitFor();
+    m1.stop();
+
+
+    opening_start = 500;
+    opening_end = 500;
+    continue_search = 0;
+
+
+    for i = 2:length(values(:,1))
+        if values(i,1) >= 45 && values(i-1,1) >= 45
+            if continue_search == 1
+                opening_end = rad2deg(values(i,3));
+            end
+
+            if opening_start == 500
+                opening_start = rad2deg(values(i,3));
+                continue_search = 1;
+            end
+        end
+    end
+
+    opening_start
+    opening_end
+
+
+    opening = (opening_end+opening_start)/2
+
+
+
+
+
+
+
+    polarplot(values(:,3), values(:,1));
+    axis padded
+
+    hold on
+
+    polarplot(deg2rad(double(opening)), 20);
+
+    pause(1)
+
+
+
+    mb.limitValue = 5000;
+    mb.power = -40;
+    mb.brakeMode = 'Brake';
+
+    mc.limitValue = 5000;
+    mc.power = 40;
+    mc.brakeMode = 'Brake';
+
+    handle.sensor3.reset();
+
+    mb.start();
+    mc.start();
+
+    while handle.sensor4.value > 45
+        pause(0.1)
+    end
+
+    pause(0.2)
+
+    mb.stop();
+    mc.stop();
+
+
+    mb.limitValue = 5000;
+    mb.power = 70;
+    mb.brakeMode = 'Brake';
+
+    mc.limitValue = 5000;
+    mc.power = 70;
+    mc.brakeMode = 'Brake';
+
+    mb.start();
+    mc.start();
+
+    while handle.sensor4.value > 20
+        pause(0.1)
+    end
+
+    mb.stop();
+    mc.stop();
+
+    handle.disconnect();
+end
