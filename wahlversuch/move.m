478c94ad14e05c1fc6654a885d34a834b2753d8d
Peter <hi@p3h3.de>
2022-12-20 13:45:58 +0100

    functions now accessible

--- wahlversuch/move.m
+++ wahlversuch/move.m
@@ -1,75 +1,79 @@
-%% bewegt den Farbsensor nach hinten
-function moveUp(handle)
-    m2 = handle.motorB;
+classdef move
+    methods(Static)
+            
+    %% bewegt den Farbsensor nach hinten
+    function moveUp(handle)
+        m2 = handle.motorB;
 
-    m2.power = 20;
-    m2.limitMode = 'tachoCount';
-    m2.limitValue = 10;
-    m2.resetTachoCount();
+        m2.power = 20;
+        m2.limitMode = 'tachoCount';
+        m2.limitValue = 10;
+        m2.resetTachoCount();
 
-    m2.start();
-    m2.waitFor();
-    m2.stop();
+        m2.start();
+        m2.waitFor();
+        m2.stop();
 
-    handle.y_Pos = y_Pos + 1;
+        handle.y_Pos = y_Pos + 1;
+
+    end
+
+
+
+    %% bewegt den Farbsensor nach vorne
+    function moveDown(handle)
+        m2 = handle.motorB;
+
+        m2.power = -20;
+        m2.limitMode = 'tachoCount';
+        m2.limitValue = 10;
+        m2.resetTachoCount();
+
+        m2.start();
+        m2.waitFor();
+        m2.stop();
+
+        handle.y_Pos = y_Pos - 1;
+
+    end
+
+
+
+    %% bewegt den Papierträger nach links
+    function moveLeft(handle)
+        m1 = handle.motorC;
+
+        m1.power = -20;
+        m1.limitMode = 'tachoCount';
+        m1.limitValue = 10;
+        m1.resetTachoCount();
+
+        m1.start();
+        m1.waitFor();
+        m1.stop();
+
+        handle.x_Pos = x_Pos - 1;
+
+    end
+
+
+
+
+    %% bewegt den Papierträger nach rechts
+    function moveRight(handle)
+        m1 = handle.motorC;
+
+        m1.power = 20;
+        m1.limitMode = 'tachoCount';
+        m1.limitValue = 10;
+        m1.resetTachoCount();
+
+        m1.start();
+        m1.waitFor();
+        m1.stop();
+
+        handle.x_Pos = x_Pos +1;
+
+    end
 
 end
-
-
-
-%% bewegt den Farbsensor nach vorne
-function moveDown(handle)
-    m2 = handle.motorB;
-
-    m2.power = -20;
-    m2.limitMode = 'tachoCount';
-    m2.limitValue = 10;
-    m2.resetTachoCount();
-
-    m2.start();
-    m2.waitFor();
-    m2.stop();
-
-    handle.y_Pos = y_Pos - 1;
-
-end
-
-
-
-%% bewegt den Papierträger nach links
-function moveLeft(handle)
-    m1 = handle.motorC;
-
-    m1.power = -20;
-    m1.limitMode = 'tachoCount';
-    m1.limitValue = 10;
-    m1.resetTachoCount();
-
-    m1.start();
-    m1.waitFor();
-    m1.stop();
-
-    handle.x_Pos = x_Pos - 1;
-
-end
-
-
-
-
-%% bewegt den Papierträger nach rechts
-function moveRight(handle)
-    m1 = handle.motorC;
-
-    m1.power = 20;
-    m1.limitMode = 'tachoCount';
-    m1.limitValue = 10;
-    m1.resetTachoCount();
-
-    m1.start();
-    m1.waitFor();
-    m1.stop();
-
-    handle.x_Pos = x_Pos +1;
-
-end
-
