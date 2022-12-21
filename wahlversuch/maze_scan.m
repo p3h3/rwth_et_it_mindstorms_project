c7cd69809e1de5ed52ecc9d445edea4f824bb923
Peter <hi@p3h3.de>
2022-12-21 10:44:10 +0100

    update für paul

--- wahlversuch/maze_scan.m
+++ wahlversuch/maze_scan.m
@@ -15,50 +15,39 @@ handle.sensor4.mode = DeviceMode.Color.Col;
 
 %pause(5);
 figure();
-plot(0,0,"diamond",'Color','b');
+%plot(0,0,"diamond",'Color','b');
 hold on
 grid on
-plot(breite+1,laenge+1,"diamond",'Color','b');
+%plot(breite+1,laenge+1,"diamond",'Color','b');
 xlim([-1 breite+2])
 ylim([-1 laenge+2])
  
 move.resetPos(handle);
 
-for y=1:2:laenge
+for y=1:1:laenge
     move.Down(handle);
     for x=1:breite
         move.Right(handle);
         scanValue = handle.sensor4.value;
+
         if scanValue == 6       %Leer
-            plot(x,y,"*",'Color','w');
+            plot(x,y,"*",'Color','y');
+            values(x,y) = 2;
+
         elseif scanValue == 1   %Barriere
             plot(x,y,"square",'Color','k');
-            values(x,y) = -1
+            values(x,y) = -1;
+
         elseif scanValue == 3   %Startpunkt
             plot(x,y,"o",'Color','g');
-            values(x,y) = 1
+            values(x,y) = 1;
+
         elseif scanValue == 5   %Ziel
             plot(x,y,"o",'Color','r');
-            values(x,y) = 0
+            values(x,y) = 0;
         end
     end
-    move.Down(handle);
-    for x=breite:-1:1
-        move.Left(handle);
-        scanValue = handle.sensor4.value;
-        if scanValue == 6       %Leer
-            plot(x,y+1,"*",'Color','w');
-        elseif scanValue == 1   %Barriere
-            plot(x,y+1,"square",'Color','k');
-            values(x,y+1) = -1
-        elseif scanValue == 3   %Startpunkt
-            plot(x,y+1,"o",'Color','g');
-            values(x,y+1) = 1
-        elseif scanValue == 5   %Ziel
-            plot(x,y+1,"o",'Color','r');
-            values(x,y+1) = 0
-        end
-    end
+    move.Left2(handle, breite + 1)
 end
 
 for y=1:laenge
@@ -66,6 +55,9 @@ for y=1:laenge
 end
 
 move.resetPos(handle); 
+
+
+
 
 values
 
--- wahlversuch/move.m
+++ wahlversuch/move.m
@@ -5,7 +5,7 @@ classdef move
     function Up(handle)
         m2 = handle.motorB;
 
-        m2.power = 20;
+        m2.power = 40;
         m2.limitMode = 'Tacho';
         m2.brakeMode = 'Brake';
         m2.limitValue = 15;
@@ -22,7 +22,7 @@ classdef move
     function Down(handle)
         m2 = handle.motorB;
 
-        m2.power = -20;
+        m2.power = -40;
         m2.limitMode = 'Tacho';
         m2.brakeMode = 'Brake';
         m2.limitValue = 15;
@@ -39,7 +39,7 @@ classdef move
     function Left(handle)
         m1 = handle.motorC;
 
-        m1.power = -20;
+        m1.power = -40;
         m1.limitMode = 'Tacho';
         m1.brakeMode = 'Brake';
         m1.limitValue = 15;
@@ -54,7 +54,7 @@ classdef move
     function Left2(handle, fields)
         m1 = handle.motorC;
 
-        m1.power = -20;
+        m1.power = -40;
         m1.limitMode = 'Tacho';
         m1.brakeMode = 'Brake';
         m1.limitValue = 18*fields;
@@ -72,7 +72,7 @@ classdef move
     function Right(handle)
         m1 = handle.motorC;
 
-        m1.power = 20;
+        m1.power = 40;
         m1.limitMode = 'Tacho';
         m1.brakeMode = 'Brake';
         m1.limitValue = 15;
@@ -88,27 +88,25 @@ classdef move
     function resetPos(handle)
         m1 = handle.motorC;
 
-        m1.power = -20;
+        m1.power = -40;
         m1.limitMode = 'Tacho';
         m1.brakeMode = 'Brake';
         m1.limitValue = 400;
         m1.resetTachoCount();
 
-        m1.start();
-        m1.waitFor();
-        m1.stop();
 
         m2 = handle.motorB;
 
-        m2.power = 20;
+        m2.power = 40;
         m2.limitMode = 'Tacho';
         m2.brakeMode = 'Brake';
         m2.limitValue = 400;
         m2.resetTachoCount();
 
-        m2.start();
-        m2.waitFor();
-        m2.stop();
+
+        m1.syncedStart(m2);
+        m1.waitFor();
+        m1.syncedStop();
 end
 end
 end
