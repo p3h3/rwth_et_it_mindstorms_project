6c16345fda4153381b227b939780c6b7beef635c
p-fo-dev <p.fo@outlook.de>
2022-12-21 14:42:23 +0100

    endlichfunktioniertes

--- wahlversuch/finalfinal.m
+++ wahlversuch/finalfinal.m
@@ -1,84 +1,22 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% A* ALGORITHM Demo
-% Interactive A* search demo
-% 04-26-2005
-%   Copyright 2009-2010 The MathWorks, Inc.
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%DEFINE THE 2-D MAP ARRAY
-MAX_X=10;
-MAX_Y=10;
-MAX_VAL=10;
-%This array stores the coordinates of the map and the 
-%Objects in each coordinate
-
-
-% Obtain Obstacle, Target and Robot Position
+MAP=2*(ones(MAX_X,MAX_Y));
 % Initialize the MAP with input values
 % Obstacle=-1,Target = 0,Robot=1,Space=2
-j=0;
-x_val = 1;
-y_val = 1;
+
 axis([1 MAX_X+1 1 MAX_Y+1])
 grid on;
 hold on;
-n=0;%Number of Obstacles
 
-% BEGIN Interactive Obstacle, Target, Start Location selection
-pause(1);
-h=msgbox('Please Select the Target using the Left Mouse button');
-uiwait(h,5);
-if ishandle(h) == 1
-    delete(h);
-end
-xlabel('Please Select the Target using the Left Mouse button','Color','black');
-but=0;
-while (but ~= 1) %Repeat until the Left button is not clicked
-    [xval,yval,but]=ginput(1);
-end
-xval=floor(xval);
-yval=floor(yval);
 xTarget=xval;%X Coordinate of the Target
 yTarget=yval;%Y Coordinate of the Target
 
 MAP(xval,yval)=0;%Initialize MAP with location of the target
-plot(xval+.5,yval+.5,'gd');
-text(xval+1,yval+.5,'Target')
 
-pause(2);
-h=msgbox('Select Obstacles using the Left Mouse button,to select the last obstacle use the Right button');
-  xlabel('Select Obstacles using the Left Mouse button,to select the last obstacle use the Right button','Color','blue');
-uiwait(h,10);
-if ishandle(h) == 1
-    delete(h);
-end
-while but == 1
-    [xval,yval,but] = ginput(1);
-    xval=floor(xval);
-    yval=floor(yval);
-    MAP(xval,yval)=-1;%Put on the closed list as well
-    plot(xval+.5,yval+.5,'ro');
- end%End of While loop
- 
-pause(1);
 
-h=msgbox('Please Select the Vehicle initial position using the Left Mouse button');
-uiwait(h,5);
-if ishandle(h) == 1
-    delete(h);
-end
-xlabel('Please Select the Vehicle initial position ','Color','black');
-but=0;
-while (but ~= 1) %Repeat until the Left button is not clicked
-    [xval,yval,but]=ginput(1);
-    xval=floor(xval);
-    yval=floor(yval);
-end
-xStart=xval;%Starting Position
-yStart=yval;%Starting Position
-MAP(xval,yval)=1;
- plot(xval+.5,yval+.5,'bo');
-%End of obstacle-Target pickup
+xStart=x2val;%Starting Position
+yStart=y2val;%Starting Position
+MAP(x2val,y2val)=1;
+
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %LISTS USED FOR ALGORITHM
@@ -222,8 +160,3 @@ else
 end
 
     
-
-
-
-
-
